e8b0b0399a1d2b00981012157c8522cef2285a87
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-11-29 10:52:16 -0800

    loops

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def to_uper(character):
     if ord(character) >= 97 and ord(character) <= 122:
-         return (ord(character) - 32)
+        return (ord(character) - 32)
     else:
         return ord(character)
 
