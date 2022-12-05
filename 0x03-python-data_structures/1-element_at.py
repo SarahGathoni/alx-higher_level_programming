acb16a18aaad60dee9494e462bed8f46fd078ac7
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-12-04 23:14:44 -0800

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/ut(my_list, idx):
+
+def element_at(my_list, idx):
+
+    if idx < 0:
+
+        return None
+
+    elif idx >= len(my_list):
+
+        return None
+
+    else:
+
+        return my_list[idx]
