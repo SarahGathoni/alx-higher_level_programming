15c7ce775b6e7ff7640b18ee7da1d6c4b1e44ed7
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-12-07 17:36:21 -0800

    prototypes

--- 0x04-python-more_data_structures/python.h
+++ 0x04-python-more_data_structures/python.h
@@ -1,2 +1,9 @@
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
+def complex_delete(a_dictionary, value):
+def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+def roman_to_int(roman_string):
+def multiply_list_map(my_list=[],  number=0):
+def best_score(a_dictionary):
+def multiply_by_2(a_dictionary):
