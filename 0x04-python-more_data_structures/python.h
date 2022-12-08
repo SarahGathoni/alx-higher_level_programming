71a45002414c74227087f97380199fd32865f9d6
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-12-07 17:57:11 -0800

    python.c

--- 0x04-python-more_data_structures/python.h
+++ 0x04-python-more_data_structures/python.h
@@ -1,9 +1,13 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 def complex_delete(a_dictionary, value):
-def square_matrix_map(matrix=[]):
 def weight_average(my_list=[]):
 def roman_to_int(roman_string):
-def multiply_list_map(my_list=[],  number=0):
+def multiply_list_map(my_list=[], number=0):
 def best_score(a_dictionary):
-def multiply_by_2(a_dictionary):
+
+#endif /* PYTHON_H */
+
