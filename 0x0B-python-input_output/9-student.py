6959d49bbccc092b8a8f16d08a2bc340c8e96ed6
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-01-11 10:01:57 -0800

    changes 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""Defines a class Student. """
 
 
 class Student:
-    """Represent a student."""
+    """ Represent a student. """
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-
-         Args:
+        Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
@@ -18,5 +17,5 @@ class Student:
         self.age = age
 
     def to_json(self):
-        """Get a dictionary representation of the Student."""
-         return self.__dict__
+        """ Get a dictionary representation of the student. """
+        return self.__dict__
